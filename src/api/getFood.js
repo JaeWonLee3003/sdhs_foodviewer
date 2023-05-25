9441be82eca4244a12c5fd1b7a42fc93a5fd371b
JaeWonLee3003 <dlwodnjs3003@gmail.com>
2023-05-25 16:27:18 +0900

    2023_05_25 class

---
+++ src/api/getFood.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const getFood = () => {
+  return axios.get("http://127.0.0.1:5000/foodTest")
+}
--- src/components/FoodList/index.js
+++ src/components/FoodList/index.js
@@ -1,24 +1,45 @@
-import { Card, Center ,CardHeader, Heading ,Text} from "@chakra-ui/react";
+import { Card, Center ,CardHeader, Heading ,Text, CardBody,Spinner} from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { getFood } from "../../api/getFood";
 
-function FoodList() {
+export function FoodList() {
+const {data, isLoading,error,isSuccess} = useQuery(["food"],
+
+()=>getFood(),{initialData:{"data":""}})
+
     return (
         <Center
             h={"100vh"}
-            justifyContent={"center"}>
+            justifyContent={"center"}
+            flexDirection={"column"}  
+            maring
+            >
+            
             <Card
             size={"md"}
             border={"1px solid #E2E8F0"}
             w={"md"}
-            textAlign={"center"}
+            textAlign={"center"}  
             >
                 
-                <CardHeader>
+                <CardHeader >
                     <Heading>오늘의 급식</Heading>
                 </CardHeader>
-                <Text>현미밥, 옹심이국, 오리주물럭, 감자전&양파간장, 애호박새우살볶음, 배추김치</Text>
+                <CardBody>
+                    {isLoading?
+                    (<Spinner/>):(<Text>{data.data["food"]}</Text>)
+                    }                    
+                </CardBody>
+
+
                 </Card>
+                <form>
+
+                <input type="text" className="review_info" name="review_info" placeholder="40자 내로 입력해주세요." ></input>
+                <input type="submit" value={"입력"}></input>
+                </form> 
         </Center>
     );
 }
 
-export default FoodList;
+export default FoodList
